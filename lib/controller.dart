b2bbb6db1a85d7191540462829b9daff8cf8003a
rizalkalam <rizalkalam78@gmail.com>
2023-02-09 07:54:09 +0700

    [RK]nambahin rider page

--- lib/controller.dart
+++ lib/controller.dart
@@ -5,9 +5,11 @@ import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:http/http.dart' as http;
 import 'model/rider_model.dart';
+import 'model/team_model.dart';
 
 class RiderController extends GetxController {
   RiderModel? RiderModels;
+  TeamModel? TeamModels;
   var isDataLoading = false.obs;
 
   @override
@@ -19,13 +21,19 @@ class RiderController extends GetxController {
   getApi() async {
     try {
       isDataLoading(true);
-      http.Response response =
+      http.Response responseRider =
           await http.get(Uri.parse("http://rizalkalam.fun/api/rider/all"));
-      if (response.statusCode == 200) {
-        ///data successfully
-        var result = jsonDecode(response.body);
 
-        RiderModels = RiderModel.fromJson(result);
+      http.Response responseTeam =
+          await http.get(Uri.parse("http://rizalkalam.fun/api/team/all"));
+
+      if (responseRider.statusCode == 200 || responseTeam.statusCode == 200) {
+        ///data successfully
+        var resultRider = jsonDecode(responseRider.body);
+        var resultTeam = jsonDecode(responseTeam.body);
+
+        RiderModels = RiderModel.fromJson(resultRider);
+        TeamModels = TeamModel.fromJson(resultTeam);
       } else {
         ///error
       }
